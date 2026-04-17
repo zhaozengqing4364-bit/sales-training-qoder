7425b1dac7bb9636734b4d6b1d25eb864d8e2a7b
Zzq <505874232@qq.com>
2026-04-17 14:25:43 +0800

    omx(team): auto-checkpoint worker-4 [4]

--- web/src/app/admin/users/[id]/page.test.tsx
+++ web/src/app/admin/users/[id]/page.test.tsx
@@ -7,6 +7,7 @@ import { buildAdminUserDrillInHref } from "@/lib/admin/drill-in";
 import type {
     LinkedAssetChangeReference,
     ManagerInterventionListResponse,
+    ManagerInterventionRemindResponse,
     SupportRuntimeFaultsResponse,
     UserProgressResponse,
     UserSessionsResponse,
