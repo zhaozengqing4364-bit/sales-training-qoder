12d1a1e7c08862f3136a12047c186aa01b3b3f60
Zzq <505874232@qq.com>
2026-04-24 19:09:48 +0800

    omx(team): auto-checkpoint worker-1 [1]

--- backend/src/admin/api/__init__.py
+++ backend/src/admin/api/__init__.py
@@ -7,6 +7,7 @@ Contains admin endpoints for:
 - System logs
 """
 from .admin import router as admin_router
+from .business_rules import router as business_rules_router
 from .knowledge_answer_config import router as knowledge_answer_config_router
 from .system_logs import router as system_logs_router
 from .training_records import router as training_records_router
@@ -14,6 +15,7 @@ from .users import router as users_router
 
 __all__ = [
     "admin_router",
+    "business_rules_router",
     "users_router",
     "training_records_router",
     "system_logs_router",
